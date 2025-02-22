4da4476b4a9339f66921ecd98bdc7794d96e09c3
rally19 <masterrally1808@gmail.com>
2025-02-22 11:53:00 +0800

    ALPHA COMMIT 4.1

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
 <?php
-$server = getenv('DB_SERVER');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$database = getenv('DB_NAME');
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "db_mamat";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 if (!$conn) {
-    error_log("Database connection failed: " . mysqli_connect_error());
-    die("An unexpected error occurred. Please try again later.");
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 ?>
